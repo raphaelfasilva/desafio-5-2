96823ba66813d2ac5719aa4158a4f17bffa0a561
raphaelfasilva <raphaelfasilva@gmail.com>
2021-03-29 20:53:43 -0300

    29/03/21

--- src/app/controller/students.js
+++ src/app/controller/students.js
@@ -1,4 +1,5 @@
 const student = require("../models/student")
+const { date } = require("../../lib/util")
 
 module.exports = {
     index(req, res) {
@@ -19,19 +20,33 @@ module.exports = {
     },
     show(req, res) {
         const { id } = req.params
+        student.find(id, function(student) {
+            if (!student) return res.send("instrutor não encontrado")
+            student.birth = date(student.birth).format
+            return res.render("students/show", { student })
+        })
         return
     },
     edit(req, res) {
         const { id } = req.params
+        student.find(id, function(student) {
+            if (!student) return res.send("instrutor não encontrado")
+            student.birth = date(student.birth).iso
+            return res.render("students/edit", { student })
+        })
         return
     },
     put(req, res) {
         const { id } = req.body
-        let index = 0
+        student.update(req.body, function() {
+            return res.redirect(`/students/${id}`)
+        })
         return
     },
     delete(req, res) {
         const { id } = req.body
-        return
+        student.delete(id, function() {
+            return res.redirect("/students")
+        })
     }
 }
--- src/app/models/student.js
+++ src/app/models/student.js
@@ -34,27 +34,29 @@ module.exports = {
         })
     },
     find(id, callback) {
-        db.query('select * from instructors where id = $1', [id], function(err, results) {
+        db.query('select * from students where id = $1', [id], function(err, results) {
             if (err) throw "data base error"
             callback(results.rows[0])
         })
     },
     update(data, callback) {
         const query = `
-           update instructors SET 
+           update students SET 
            avatar_url=($1),
            name=($2),
            birth=($3),
-           gender=($4),
-           services=($5)
-           where id= $6
+           email=($4),
+           schoolyear=($5),
+           hours=($6)
+           where id= $7
         `
         const values = [
             data.avatar_url,
             data.name,
             date(data.birth).iso,
-            data.gender,
-            data.services,
+            data.email,
+            data.schoolyear,
+            data.hours,
             data.id
         ]
         db.query(query, values, function(err, res) {
@@ -64,7 +66,7 @@ module.exports = {
 
     },
     delete(id, callback) {
-        db.query(`DELETE FROM instructors where id = $1`, [id], function() {
+        db.query(`DELETE FROM students where id = $1`, [id], function() {
             callback()
         })
     }
